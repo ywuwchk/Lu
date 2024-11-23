fcdd1eb07836aed4b16d13ef89f96f02a096aec9
ywuwchk <148181741+ywuwchk@users.noreply.github.com>
2024-11-22 19:51:38 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const loginForm = document.getElementById('loginForm');
+  const loginButton = document.getElementById('login-button');
 
-  loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('emailInput').value;
-    const password = document.getElementById('passwordInput').value;
+  loginButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    
+    // Accessing inputs directly by name attribute because IDs are duplicated
+    const emailInput = document.getElementsByName('userInput')[0];
+    const passwordInput = document.getElementsByName('userInput')[1];
 
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    // Perform the fetch request to the Flask login endpoint
     fetch('http://localhost:3000/login', {
       method: 'POST',
       headers: {
